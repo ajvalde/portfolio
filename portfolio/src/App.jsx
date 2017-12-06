7fe7ad540e1349a4df3dcaf7464a30dfc56b77bd
Anthony <anthonyguitarman13@gmail.com>
2017-12-06 11:46:54 -0700

    Added about component

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import React , { Component } from 'react';
-
+import About from './about';
 
 class App extends Component {
     render() {
@@ -17,6 +17,7 @@ class App extends Component {
             </div>
           </nav>
            <div>HEY</div>
+            <About/>
             </div>
         )
     }
---
+++ portfolio/src/about.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class About extends Component {
+    render () {
+        return (
+            <div>
+                <div class="row">
+                    <div class="col s12 m5">
+                        <div class="card-panel blue">
+                            <span class="white-text">About Me!</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default About;
