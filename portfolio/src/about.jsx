e976421760af515b2e9bbb21ed9e72958c7d4cd4
Anthony <anthonyguitarman13@gmail.com>
2017-12-08 11:09:14 -0700

    added css

--- portfolio/src/about.jsx
+++ portfolio/src/about.jsx
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
+import './App.css';
 
 class About extends Component {
     render () {
         return (
-            <div>
-                <div class="row">
-                    <div class="col s12 m5">
+            <div class="container">
+                <div class="acard row section">
+                    <div class="col s5">
                         <div class="card-panel blue">
-                            <span class="white-text">About Me!</span>
+                            <span class="white-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor, nunc pulvinar tristique faucibus, 
+                            velit ex vehicula sem, ac iaculis diam velit at mauris. Vivamus at elit lorem. Donec pharetra lectus sed lorem maximus facilisis. 
+                            Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut ornare risus a ultrices tincidunt.
+                             Integer dapibus at ipsum et lobortis. Morbi ut volutpat magna. Maecenas enim nulla, aliquam ultricies orci id, sagittis aliquam erat. In sed eleifend ante.
+                             </span>
                         </div>
                     </div>
 
