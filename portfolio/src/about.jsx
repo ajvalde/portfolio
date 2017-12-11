0f1ec6dd9a999ff4364aeb83d33c0ab5b871f32a
Anthony <anthonyguitarman13@gmail.com>
2017-12-11 11:27:18 -0700

    one more test

--- portfolio/src/about.jsx
+++ portfolio/src/about.jsx
@@ -11,7 +11,7 @@ class About extends Component {
                             <span class="black-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam auctor, nunc pulvinar tristique faucibus, 
                             velit ex vehicula sem, ac iaculis diam velit at mauris. Vivamus at elit lorem. Donec pharetra lectus sed lorem maximus facilisis. 
                             Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut ornare risus a ultrices tincidunt.
-                             Integer dapibus at ipsum et lobortis. Morbi ut volutpat magna. Maecenas test enim nulla, aliquam ultricies orci id, sagittis aliquam erat. In sed eleifend ante.
+                             Integer dapibus at ipsum et lobortis. Morbi ut volutpat magna. Maecenas testing enim nulla, aliquam ultricies orci id, sagittis aliquam erat. In sed eleifend ante.
                              </span>
                         </div>
                     </div>
