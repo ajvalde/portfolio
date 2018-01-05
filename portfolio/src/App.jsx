8243444499b34c48088804350ec3ee1f536b2d2f
Anthony <anthonyguitarman13@gmail.com>
2018-01-05 00:06:46 -0700

    added media queries

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -12,11 +12,16 @@ class App extends Component {
             isHidden: true
         }
     }
+
     toggleHidden () {
         this.setState({
             isHidden: !this.state.isHidden
         })
     }
+
+    // $(function() {
+    //     // Handler for .ready() called.
+    //   });
 
     render() {
         return (
@@ -28,10 +33,16 @@ class App extends Component {
             <nav class="top-nav fixed-nav">
             <div class="nav-wrapper z-depth-2 ">
               <a href="#" class="brand-logo center black-text"></a>
+              <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
               <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li><a class="btn">Sass</a></li>
                 <li><a class="btn">Components</a></li>
                 <li><a class="btn">JavaScript</a></li>
+              </ul>
+              <ul class="side-nav" id="mobile-demo">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">Javascript</a></li>
               </ul>
             </div>
           </nav>
@@ -47,7 +58,7 @@ class App extends Component {
                 </div>
                 {/* <Logo/> */}
                 <About/>
-                <Projects/>
+                {/* <Projects/> */}
                 
 
             </div>
