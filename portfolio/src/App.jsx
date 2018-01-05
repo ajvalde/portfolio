25b031f46589feedf706e2176995696b6b731d68
Anthony <anthonyguitarman13@gmail.com>
2018-01-05 10:26:59 -0700

    Added Footer.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import React , { Component } from 'react';
 import About from './about';
-import Sidenav from './sidenav';
+import Footer from './Footer';
 import Logo from './Logo';
 import Projects from './project'
 import './App.css'
@@ -26,65 +26,44 @@ class App extends Component {
     render() {
         return (
             <div>
-
-            <div class="top-nav">
-
-
-            <nav class="top-nav fixed-nav">
-            <div class="nav-wrapper z-depth-2 ">
-              <a href="#" class="brand-logo center black-text"></a>
-              <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-              <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a class="btn">Sass</a></li>
-                <li><a class="btn">Components</a></li>
-                <li><a class="btn">JavaScript</a></li>
-              </ul>
-              <ul class="side-nav" id="mobile-demo">
-                <li><a href="sass.html">Sass</a></li>
-                <li><a href="badges.html">Components</a></li>
-                <li><a href="collapsible.html">Javascript</a></li>
-              </ul>
-            </div>
-          </nav>
-            </div>
-          
-          <div class="row">
-          
-            
-
-            <div class="card-panel content col s12">
-                <div class="logo">
-                    <img src={require('./img/my_logo3.png')} alt="" height="176"/>
-                </div>
-                {/* <Logo/> */}
-                <About/>
-                {/* <Projects/> */}
-                
-
-            </div>
-
-            {/* footer */}
-            <footer class="page-footer foot">
-                <div class="container">
-                    <div class="row">
-                    <div class="col l6 s12">
-                        <h5 class="white-text">Footer Content</h5>
-                        <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-                    </div>
-                    <div class="col l4 offset-l2 s12">
-                        <h5 class="white-text">Links</h5>
-                        <ul>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                    {/* nav */}
+                <div class="top-nav">
+                    <nav class="top-nav fixed-nav">
+                        <div class="nav-wrapper z-depth-2 ">
+                        <a href="#" class="brand-logo center black-text"></a>
+                        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+                        <ul id="nav-mobile" class="right hide-on-med-and-down">
+                            <li><a class="btn">Sass</a></li>
+                            <li><a class="btn">Components</a></li>
+                            <li><a class="btn">JavaScript</a></li>
                         </ul>
-                    </div>
-                    </div>
+                        <ul class="side-nav" id="mobile-demo">
+                            <li><a href="sass.html">Sass</a></li>
+                            <li><a href="badges.html">Components</a></li>
+                            <li><a href="collapsible.html">Javascript</a></li>
+                        </ul>
+                        </div>
+                    </nav>
                 </div>
-             </footer>
-        
-          </div>
+            
+                {/* content */}
+                <div class="row">
+                
+                    <div class="card-panel content col s12 z-depth-2">
+                        <div class="logo">
+                            <img src={require('./img/my_logo3.png')} alt="" height="176"/>
+                        </div>
+                        {/* <Logo/> */}
+                        <About/>
+                        {/* <Projects/> */}
+                        
+
+                    </div>
+
+                    {/* footer */}
+                    <Footer/>
+                
+                </div>
             </div>
         )
     }
---
+++ portfolio/src/topnav.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Topnav extends Component {
+    render () {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Topnav
