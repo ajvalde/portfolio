a982b181b5947ce9d8af538fa8320a2a28cd9529
Anthony <anthonyguitarman13@gmail.com>
2018-01-04 19:41:46 -0700

    added footer

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -24,7 +24,7 @@ class App extends Component {
 
             <nav class="top-nav fixed-nav">
             <div class="nav-wrapper z-depth-2 ">
-              <a href="#" class="brand-logo center black-text"><img src={require('./img/my_logo3.png')} alt="" height="176"/></a>
+              <a href="#" class="brand-logo center black-text"></a>
               <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li><a class="btn">Sass</a></li>
                 <li><a class="btn">Components</a></li>
@@ -34,15 +34,45 @@ class App extends Component {
           </nav>
           
           <div class="row">
+          
             
 
             <div class="card-panel content col s12">
-
+                <div class="logo">
+                    <img src={require('./img/my_logo3.png')} alt="" height="176"/>
+                </div>
                 {/* <Logo/> */}
                 <About/>
-                <Projects/>
+                {/* <Projects/> */}
 
             </div>
+
+            {/* footer */}
+            <footer class="page-footer foot">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          {/* <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div> */}
+        </footer>
           </div>
             </div>
         )
