1ac5e93b0bad27e42de1684eab6aeede87a3ba93
Anthony <anthonyguitarman13@gmail.com>
2018-02-23 10:31:38 -0700

    some more work

--- portfolio-v2/src/App.js
+++ portfolio-v2/src/App.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
 import { Navbar, Button, Row, Col, NavItem } from 'react-materialize';
-import logo from './logo.svg';
 import './App.css';
+import Projects from './Projects';
+
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar brand='hey' right>
-          <NavItem href='get-started.html'>Getting started</NavItem>
-          <NavItem href='components.html'>Components</NavItem>
+        <Navbar brand="Anthony"class="Navbar" right>
+          {/* <Link to="/projects"><NavItem>Projects</NavItem></Link> */}
+          <Link to=""><NavItem>Skills</NavItem></Link>
+          <NavItem>Hey</NavItem>
         </Navbar>
+
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/projects" component={Projects}/>
+      </Switch>
+
       </div>
+
+
     );
   }
 }
