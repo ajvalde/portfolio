abae3332ecba4632ba5437174fa285fb7dd7d005
Anthony <anthonyguitarman13@gmail.com>
2018-03-01 12:00:43 -0700

    image not loading yet

--- portfolio-v2/src/Home.jsx
+++ portfolio-v2/src/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Navbar, Button, Row, Col, NavItem, Card, CardTitle } from 'react-materialize';
 
+
+
 const Home = ({ match }) => {
+    let pic = <img src={require('./headshot.jpg')} /> 
     return (
         <div class="container">
             <div class="about">
-                <Card header={<CardTitle reveal image={"./logo.svg"} waves='light'/>}
+                <Card header={<CardTitle reveal image={pic} waves='light'/>}
                 title="About Me"
                 reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
                 </Card>
