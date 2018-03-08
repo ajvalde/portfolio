96c296e414629d629e30f944d332d90b3d822684
Anthony <anthonyguitarman13@gmail.com>
2018-03-08 11:53:53 -0700

    added tools

--- portfolio-v2/src/Skills.jsx
+++ portfolio-v2/src/Skills.jsx
@@ -9,7 +9,7 @@ const Skills = ({match}) => {
             <div class="row">
                 <div class="col m8 s12 offset-m2 card-panel black white-text ">
                     <h5 class="center-align">Languages/Frameworks</h5>
-                    
+
                         <div class="row center-align">
                             <div class="col s6">
                                 <p>Vue.js</p>
@@ -19,7 +19,6 @@ const Skills = ({match}) => {
                                 <p>PostgreSQL</p>
                             </div>
 
-                      
                             <div class="col s6">
                                 <p>JavaScript</p>
                                 <p>Ruby on Rails</p>
@@ -28,14 +27,29 @@ const Skills = ({match}) => {
                                 <p>Reactjs</p>
                             </div>
                         </div>
-                        
     
                 </div>
              </div>
 
         <div class="row">
             <div class="col m8 s12 offset-m2 card-panel black white-text">
-                 <h5 class="center-align">Tools</h5>   
+                 <h5 class="center-align">Tools</h5>
+
+                    <div class="row center-align">
+                        <div class="col s6">
+                            <p>Git</p>
+                            <p>Github</p>
+                            <p>Postman</p>
+                        </div>
+
+                        <div class="col s6">
+                            <p>Pivotl Tracker</p>
+                            <p>Heroku</p>
+                            <p>Firebase</p>
+                        </div>
+
+                    </div>  
+
             </div>
         </div>
 
