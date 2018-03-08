0e3bf9e7105a0b98a925c6b1a5bb0d33e0ab9707
Anthony <anthonyguitarman13@gmail.com>
2018-03-07 18:00:38 -0700

    aligned skill cards

--- portfolio-v2/src/Skills.jsx
+++ portfolio-v2/src/Skills.jsx
@@ -4,22 +4,26 @@ import { Table, Card } from 'react-materialize';
 const Skills = ({match}) => {
     return (
         <div class="container">
-        <h2 class="header-tag">Skills</h2>
 
-        <div class="card-panel black white-text">
-        <h5 class="center-align">Languages/Frameworks</h5>
-            
+         <h2 class="header-tag">Skills</h2>
+
+            <div class="row">
+                <div class="col m8 s12 offset-m2 card-panel black white-text ">
+                    <h5 class="center-align">Languages/Frameworks</h5>
+                </div>
+             </div>
+
+        <div class="row">
+            <div class="col m8 s12 offset-m2 card-panel black white-text">
+                 <h5 class="center-align">Tools</h5>   
+            </div>
         </div>
 
-       <div class="card-panel black white-text">
-            <h5 class="center-align">Tools</h5>
-            
+        <div class="row">
+            <div class="col m8 s12 offset-m2 card-panel black white-text">
+                <h5 class="center-align">Techniques</h5>
+            </div>
         </div>
-
-        <div class="card-panel black white-text">
-        <h5 class="center-align">Techniques</h5>
-        
-    </div>
 
 
         </div>
