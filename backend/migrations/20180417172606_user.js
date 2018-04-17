97c96899d9cdc528fe2cf16e07abdc1e943695be
Anthony <anthonyguitarman13@gmail.com>
2018-04-17 17:36:08 -0600

    fixed migrations

--- backend/migrations/20180417172606_user.js
+++ backend/migrations/20180417172606_user.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("user", function (table) {
-      table.increments('id').primary();
+  return knex.schema.createTable('user', function (table) {
+      table.integer('id').primary();
       table.string('name');
       table.string('password');
   })
